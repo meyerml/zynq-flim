177d8eee0a816792c7e2f19fd7dfc749e58f58a2
marcel meyer <mameyer@physik.uni-kl.de>
2026-02-24 09:06:42 +0100

    python scripts

---
+++ python/.ipynb_checkpoints/binary_data-checkpoint.c
@@ -0,0 +1,18 @@
+// Auto-generated C array from PNG
+#include <stdint.h>
+
+const uint32_t binary_data[] = {
+    0x00000000,
+    0x0000000F,
+    0x00000000,
+
+    0x0000000F,
+    0x00000000,
+    0x0000000F,
+    
+    0x00000000,
+    0x0000000F,
+    0x00000000,
+
+};
+const size_t binary_data_size = 3;
